861e0972a338c29cbe695b6d02c4c393cca298db
rgabrik1029-png <rgabrik1029@gmail.com>
2026-06-11 10:45:51 -0700

    Add files via upload
    Updated Version

--- bridgeMotorControl.ino
+++ bridgeMotorControl.ino
@@ -1,14 +1,31 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include "Wire.h" // This library allows you to communicate with I2C devices.
+// (c) Michael Schoeffler 2017, http://www.mschoeffler.de
+
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+int16_t temperature; // variables for temperature data
+
+char tmp_str[7]; // temporary variable used in convert function
+
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
 
 // ---------- Pin setup ----------
 // For an RC plane-style brushless motor, the Arduino normally drives the ESC
 // with a servo-style signal, not by powering the motor directly.
-const int SPEED_CONTROL = 13;
+const int SPEED_CONTROL = 11;
 const int IN1 = 12;
-const int IN2 = 11;
+const int IN2 = 13;
 const int SERVO1_PIN = 4;
 const int SERVO2_PIN = 5;
+const int SERVO3_PIN = 6;
+const int SERVO4_PIN = 7;
 
 // ESC pulse range. Many ESCs use 1000 us = off, 2000 us = full throttle.
 // Calibrate your ESC separately if needed.
@@ -38,9 +55,10 @@ struct ControllerState {
 
 ControllerState state;
 String rxLine;
-Servo esc;
 Servo servo1;
 Servo servo2;
+Servo servo3;
+Servo servo4;
 unsigned long lastPacketMs = 0;
 
 int readIntField(const String &src, const char *key, int fallback) {
@@ -127,14 +145,20 @@ void applyCommands() {
   // ---------- Servos ----------
   int servo1Angle = axisToServoAngle(state.rx);
   int servo2Angle = axisToServoAngle(state.ry);
+  int servo3Angle = axisToServoAngle(state.rx);
+  int servo4Angle = axisToServoAngle(state.ry);
 
   if (state.l1) {
     servo1Angle = SERVO_CENTER_DEG;
     servo2Angle = SERVO_CENTER_DEG;
+    servo3Angle = SERVO_CENTER_DEG;
+    servo4Angle = SERVO_CENTER_DEG;
   }
-
+  //Writes ouput to servos
   servo1.write(servo1Angle);
   servo2.write(servo2Angle);
+  servo3.write(servo3Angle);
+  servo4.write(servo4Angle);
 
   // ---------- Debug output ----------
   Serial.print("ACK motor_pwm=");
@@ -170,15 +194,26 @@ void setup() {
   pinMode(IN2, OUTPUT);
   servo1.attach(SERVO1_PIN);
   servo2.attach(SERVO2_PIN);
+  servo3.attach(SERVO1_PIN);
+  servo4.attach(SERVO2_PIN);
 
   servo1.write(SERVO_CENTER_DEG);
   servo2.write(SERVO_CENTER_DEG);
-
+  servo3.write(SERVO_CENTER_DEG);
+  servo4.write(SERVO_CENTER_DEG);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  byte error = Wire.endTransmission(true);
+  Serial.print("Wakeup error = ");
+  Serial.println(error);
+  Serial.print("Using address 0x");
+  Serial.println(MPU_ADDR, HEX);
   // Give the ESC time to see minimum throttle on startup.
-  delay(2000);
-
+  delay(3000);
   lastPacketMs = millis();
-  Serial.println("Arduino bridge ready: ESC pin 9, servo pins 10/11");
+  Serial.println("Arduino bridge ready: ");
 }
 
 void loop() {
@@ -200,11 +235,37 @@ void loop() {
       }
     }
   }
-
   // Failsafe: if computer/controller bridge disconnects, stop the motor.
   if (millis() - lastPacketMs > FAILSAFE_MS) {
     analogWrite(SPEED_CONTROL, 0);
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
   }
+  //gyroscope and accelorometer code
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+  Serial.print("Available: ");
+  Serial.println(Wire.available());
+  
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  
+  // print out data
+  Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
+  Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
+  Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
+  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+  Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
+  Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
+  Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
+  Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
+  Serial.println();
 }
